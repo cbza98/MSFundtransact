8072dd8023f01b408d950f4738f00897a1d5d851
Leonardo Ballestero <ballesteroleonardojose@gmail.com>
2022-05-18 17:50:59 -0500

    Ajustes Iniciales

--- src/main/java/com/bankntt/MSFundtransact/domain/entities/Credit.java
+++ src/main/java/com/bankntt/MSFundtransact/domain/entities/Credit.java
@@ -1,5 +1,34 @@
 package com.bankntt.MSFundtransact.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.Date;
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Credit {
+    @Id
+    @JsonIgnore
+    private String creditid;
+    @NotNull
+    private String codebusinesspartner;
+    @JsonIgnore
+    private Date createdate;
+    @Digits(integer = 19,fraction = 6)
+    private BigDecimal amountcredit;
+    @Digits(integer = 19,fraction = 6)
+    @JsonIgnore
+    private BigDecimal usedcredit;
+    @JsonIgnore
+    @Digits(integer = 19,fraction = 6)
+    private BigDecimal availablecredit;
 
 }
--- src/main/java/com/bankntt/MSFundtransact/domain/repository/SignerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bankntt.MSFundtransact.domain.repository;
-
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-import com.bankntt.MSFundtransact.domain.entities.Signer;
-
-public interface SignerRepository extends ReactiveMongoRepository<Signer, String> {
-
-}
